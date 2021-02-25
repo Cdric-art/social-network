23e12601447102fd511ead66adc714e994fe6f88
Cdric-art <cdric.art@gmail.com>
2021-02-25 19:01:24 +0100

    Fix: User controller clear notifications

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -11,72 +11,72 @@ module.exports.getUser = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id)) {
 		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
-	await UserModel.findOne({ _id: req.params.id })
+	await UserModel.findOne({_id: req.params.id})
 		.then(user => res.status(200).json(user))
-		.catch(err => res.status(400).json({ err }));
+		.catch(err => res.status(400).json({err}));
 };
 
 module.exports.updateUser = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id)) {
 		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
-	await UserModel.findOneAndUpdate({ _id: req.params.id },
+	await UserModel.findOneAndUpdate({_id: req.params.id},
 		{
 			...req.body
 		})
-		.then(() => res.status(200).json({ message: 'Utilisateur mis à jour' }))
-		.catch(err => res.status(400).json({ err }));
+		.then(() => res.status(200).json({message: 'Utilisateur mis à jour'}))
+		.catch(err => res.status(400).json({err}));
 };
 
 module.exports.deleteUser = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id)) {
 		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
-	await UserModel.deleteOne({ _id: req.params.id })
-		.then(() => res.status(200).json({ message: 'Utilisateur supprimé ' }))
-		.catch(err => err.status(400).json({ err }));
+	await UserModel.deleteOne({_id: req.params.id})
+		.then(() => res.status(200).json({message: 'Utilisateur supprimé '}))
+		.catch(err => err.status(400).json({err}));
 };
 
 module.exports.follow = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToFollow)) {
 		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
-	await UserModel.findOneAndUpdate({ _id: req.params.id },
+	await UserModel.findOneAndUpdate({_id: req.params.id},
 		{
-			$push: { following : req.body.idToFollow }
+			$push: {following: req.body.idToFollow}
 		});
-	await UserModel.findOneAndUpdate({ _id: req.body.idToFollow },
+	await UserModel.findOneAndUpdate({_id: req.body.idToFollow},
 		{
-			$push: { followers: req.params.id, notifications: req.params.id }
+			$push: {followers: req.params.id, notifications: req.params.id}
 		})
-		.then(() => res.status(200).json({ message: 'User follow' }))
-		.catch(err => res.status(400).json({ err }));
+		.then(() => res.status(200).json({message: 'User follow'}))
+		.catch(err => res.status(400).json({err}));
 };
 
 module.exports.unfollow = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id) || !ObjectID.isValid(req.body.idToUnfollow)) {
 		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
-	await UserModel.findOneAndUpdate({ _id: req.params.id },
+	await UserModel.findOneAndUpdate({_id: req.params.id},
 		{
-			$pull: { following : req.body.idToUnfollow }
+			$pull: {following: req.body.idToUnfollow}
 		});
-	await UserModel.findOneAndUpdate({ _id: req.body.idToUnfollow },
+	await UserModel.findOneAndUpdate({_id: req.body.idToUnfollow},
 		{
-			$pull: { followers: req.params.id }
+			$pull: {followers: req.params.id}
 		})
-		.then((doc) => res.status(200).json({ doc }))
-		.catch(err => res.status(400).json({ err }));
+		.then((doc) => res.status(200).json({doc}))
+		.catch(err => res.status(400).json({err}));
 };
 
 module.exports.clearNotifications = async (req, res) => {
 	if (!ObjectID.isValid(req.params.id)) {
-		return res.status(400).send('Id unknown : ' + req.params.id)
+		return res.status(400).send('Id unknown : ' + req.params.id);
 	}
 	await UserModel.findOneAndUpdate({_id: req.params.id},
 		{
-			notifications: ''
+			notifications: []
 		})
-		.then(() => res.status(200).json({ message: 'Notifications clear' }))
-		.catch(err => res.status(400).json({ err }));
-}
+		.then(() => res.status(200).json({message: 'Notifications clear'}))
+		.catch(err => res.status(400).json({err}));
+};
