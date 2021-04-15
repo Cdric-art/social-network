d174efb90f8e578e90207f7998affba1ad6ba052
Cdric-art <cdric.art@gmail.com>
2021-04-15 16:32:45 +0200

    Feat: search user

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const SearchBar = () => {
 
@@ -11,20 +12,25 @@ const SearchBar = () => {
 		let value = inputRef.current.value;
 		allUsers.map(user => {
 			let pseudo = user.pseudo.trim().toLowerCase();
-			value && pseudo.startsWith(value) && !searchUser.includes(user.pseudo)
-				? setSearchUser([user.pseudo, ...searchUser])
+			let matches = searchUser.find(u => u.pseudo === user.pseudo);
+			value && pseudo.startsWith(value) && !matches
+				? setSearchUser([...searchUser, user])
 				: null;
 		});
 	};
 
 	return (
 		<div className="search-bar">
-			<input ref={inputRef} onChange={handleSearch} onKeyDown={e => e.key === 'Backspace' ? setSearchUser([]) : null} type="search" name="search" placeholder="Recherche..."/>
+			<input ref={inputRef} onChange={handleSearch}
+				   onKeyDown={e => e.key === 'Backspace' ? setSearchUser([]) : null}
+				   type="search" name="search" placeholder="Recherche..."/>
 			{
 				searchUser.length > 0 ? (
 					<ul className="search-list">
-						{searchUser.map(u => (
-							<li key={u}>{u}</li>
+						{searchUser.map(user => (
+							<li key={user._id}>
+								<Link to={`/public-profil/${user._id}`}>{user.pseudo}</Link>
+							</li>
 						))}
 					</ul>
 				) : null
